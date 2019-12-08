d558c81accb04dae44e57fb1184ce072f8ae3349
tester-2019-bydgoszcz-18 <tst229bdg@gmail.com>
2019-12-08 14:19:38 +0100

    dopisanie stufu

--- intro_selenium.py
+++ intro_selenium.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# added stuff
+
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
